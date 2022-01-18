1081ec64a0cc6498fc6f7689d1249ad22d7f4df0
Jinhyun Park <developerjhp@gmail.com>
2022-01-19 03:58:49 +0900

    [Feat] 초기세팅

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Carousel } from './components/Carousel/Carousel';
+import { Navbar } from './components/Navbar/Navbar';
+import './style/global-style.css';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Carousel />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const MainBar = styled.div`
+  height: 50px;
+  border: 1px solid red;
+`;
+
+export const MainBarNav = styled.div`
+  max-width: 1060px;
+  margin: 0 auto;
+  height: 50px;
+  border: 1px solid blue;
+`;
+
+export const MainBarNavTopLogo = styled.div``;
+
+export const Hamburger = styled.div`
+  img {
+    width: 17px;
+  }
+`;
